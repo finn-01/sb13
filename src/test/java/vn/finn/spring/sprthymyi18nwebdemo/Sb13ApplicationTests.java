45d7da59dc2eba96422f23ad2a667bf295374fec
finn-01 <nguyensam576@gmail.com>
2022-02-17 22:26:59 +0700

    set up

---
+++ src/test/java/vn/finn/spring/sprthymyi18nwebdemo/Sb13ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.sprthymyi18nwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
