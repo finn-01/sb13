d5c579a57c30e7f723965844a0fc8809db63ae05
finn-01 <nguyensam576@gmail.com>
2022-02-17 23:05:45 +0700

    config

--- src/main/java/vn/finn/spring/sprthymyi18nwebdemo/config/TodoConfig.java
+++ src/main/java/vn/finn/spring/sprthymyi18nwebdemo/config/TodoConfig.java
@@ -1,4 +1,17 @@
 package vn.finn.spring.sprthymyi18nwebdemo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import vn.finn.spring.sprthymyi18nwebdemo.model.TodoValidator;
+
+@Configuration
 public class TodoConfig {
+    /**
+     * Tạo ra Bean TodoValidator để sử dụng sau này
+     * @return
+     */
+    @Bean
+    public TodoValidator validator(){
+        return new TodoValidator();
+    }
 }
