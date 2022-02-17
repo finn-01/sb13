85742146440e5ff524a7599fce1622f7d1a4db8c
finn-01 <nguyensam576@gmail.com>
2022-02-17 22:54:25 +0700

    model and config application.properties

--- src/main/java/vn/finn/spring/sprthymyi18nwebdemo/model/TodoValidator.java
+++ src/main/java/vn/finn/spring/sprthymyi18nwebdemo/model/TodoValidator.java
@@ -1,4 +1,22 @@
 package vn.finn.spring.sprthymyi18nwebdemo.model;
 
+import org.thymeleaf.util.StringUtils;
+
+import java.util.Optional;
+
+/*
+Đối tượng này dùng để kiểm tra xem một Object Todo có hợp lệ không
+ */
 public class TodoValidator {
+    /**
+     * Kiểm tra một object Todo có hợp lệ không
+     * @param todo
+     * @return
+     */
+    public boolean isInvalid(Todo todo){
+        return Optional.ofNullable(todo)
+                .filter(todo1 -> !StringUtils.isEmpty(todo1.getTitle()))
+                .filter(todo1 -> !StringUtils.isEmpty(todo1.getDetail()))
+                .isPresent(); // Trả về true nếu hợp lệ, ngược lại false
+    }
 }
